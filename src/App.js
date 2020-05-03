fd4a7b78f00d726a73787e0e36ece64d7444125d
wrunion <winterrunion@gmail.com>
2020-05-03 15:03:40 -0700

    Add working Counter compoent.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
+/* eslint-disable default-case */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 // import RollDice from './RollDice';
 import Counter from './Counter/Counter';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', action);
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count -1
+      }  ;
+    default: 
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+// console.log('created', store.getState());
+// store.dispatch({type: 'INCREMENT'});
+// console.log(store.getState());
+// store.dispatch({type: 'INCREMENT'});
+// console.log(store.getState());
+// store.dispatch({type: 'DECREMENT'});
+// console.log(store.getState());
+// store.dispatch({type: 'INCREMENT'});
+// console.log('done', store.getState());
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      {/* <RollDice />
-       */}
-       <Counter />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import './Counter.css';
+import { connect } from 'react-redux';
 
-export default class Counter extends Component {
-  state = {
-    count: 0
-  }
+class Counter extends Component {
 
   increment = () => {
-    this.setState({count: this.state.count + 1});
+    this.props.dispatch({type: 'INCREMENT'});
   };
 
   decrement = () => {
-    this.setState(state => ({
-      count: state.count -1
-    }));
+    this.props.dispatch({type: 'DECREMENT'});
   }
 
   render() {
@@ -22,10 +18,18 @@ export default class Counter extends Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span className="count">{this.state.count}</span>
+          <span className="count">{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  }
+}
+
+export default connect(mapStateToProps)(Counter);
